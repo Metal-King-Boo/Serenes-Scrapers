2077bbbb28bd9941256776295abd3eff4dc30d22
metal-king-boo <faizu901@yahoo.com>
2024-01-23 18:10:19 -0500

    cleaned up code into functions for modularity

--- serenesScrape.py
+++ serenesScrape.py
@@ -2,68 +2,67 @@ import requests
 import pymongo
 import json
 
-# grab the URL of the webpage we are scraping
-URL = "https://serenesforest.net/binding-blade/classes/base-stats/"
-page = requests.get(URL)
-# this is a string
-fullPage = page.text
 
-splitText = fullPage.split("<tr>")
-# 0-71 for this particular page
-# pop the first two items which don't have data we want
-# the page should now have 0-69 which needs more scraping
-splitText.pop(0)
-splitText.pop(0)
-# pop the intermediate data we don't need
-# the page should now have 0-66
-splitText.pop(20)
-splitText.pop(40)
-splitText.pop(60)
+def get_data_from_page(site):
+    page = requests.get(site)
+    page_string = page.text
+    split_text = page_string.split("<tr>")
+    # 0-71 for this particular page
+    # pop the first two items which don't have data we want
+    # the page should now have 0-69 which needs more scraping
+    split_text.pop(0)
+    split_text.pop(0)
+    # pop the intermediate data we don't need
+    # the page should now have 0-66
+    split_text.pop(20)
+    split_text.pop(40)
+    split_text.pop(60)
 
-# init the three lists needed
-nameList = [None] * len(splitText)
-statList = [None] * len(splitText)
-subList = []
+    name_list = [None] * len(split_text)
+    stat_list = [None] * len(split_text)
+    sub_list = []
 
-# this loop reads all the titles of the classes in each row
-for x in range(len(splitText)):
-    titleText = splitText[x]
-    titleText = titleText.splitlines()
-    titleText = titleText[1].lstrip("<td>")
-    titleText = titleText.rstrip("</\td>")
-    titleText = titleText.rstrip("t")
-    titleText = titleText.rstrip("</")
+    # this loop reads all the titles of the classes in each row
+    for x in range(len(split_text)):
+        title_text = split_text[x]
+        title_text = title_text.splitlines()
+        title_text = title_text[1].lstrip("<td>")
+        title_text = title_text.rstrip("</\td>")
+        title_text = title_text.rstrip("t")
+        title_text = title_text.rstrip("</")
 
-    # add the names to the big list
-    nameList[x] = titleText
+        # add the names to the big list
+        name_list[x] = title_text
 
-# fixing names that have changed with localization
-nameList[56] = "Valkyrie"
-nameList[57] = "Manakete (M)"
-nameList[58] = "Manakete (F)"
+    # fixing names that have changed with localization
+    name_list[56] = "Valkyrie"
+    name_list[57] = "Manakete (M)"
+    name_list[58] = "Manakete (F)"
 
-# outer loop will run for the whole table
-for x in range(len(splitText)):
-    # take the first element(list) of the nested list
-    statText = splitText[x]
-    statText = statText.splitlines()
-    # pop the excess data
-    statText.pop(0)
-    statText.pop(0)
+    # outer loop will run for the whole table
+    for x in range(len(split_text)):
+        # take the first element(list) of the nested list
+        stat_text = split_text[x]
+        stat_text = stat_text.splitlines()
+        # pop the excess data
+        stat_text.pop(0)
+        stat_text.pop(0)
 
-    # this loop creates a list with just the stat values from the table
-    for y in range(8):
-        subList.append(statText.pop(0))
+        # this loop creates a list with just the stat values from the table
+        for y in range(8):
+            sub_list.append(stat_text.pop(0))
 
-    # this loop that strips the HTML tags from the data and puts it back into the list
-    for y in range(len(subList)):
-        test = subList[y].lstrip("<td>")
-        test = test.rstrip("</td>")
-        subList[y] = test
+        # this loop that strips the HTML tags from the data and puts it back into the list
+        for y in range(len(sub_list)):
+            test = sub_list[y].lstrip("<td>")
+            test = test.rstrip("</td>")
+            sub_list[y] = test
 
-    # add the list into a bigger list then clear subList
-    statList[x] = subList
-    subList = []
+        # add the list into a bigger list then clear subList
+        stat_list[x] = sub_list
+        sub_list = []
+
+    return name_list, stat_list
 
 
 def send_json_to_mongodb(json_data, collection_name):
@@ -82,20 +81,18 @@ def send_json_to_mongodb(json_data, collection_name):
     print("Document inserted with ID:", result.inserted_id)
 
 
-# testing space
-
 # create a json with the data given to be sent to a collection
-def create_class_json(name, stats, growthList, weapon, unit_type, unit_race):
+def create_class_json(name, stats, growths, wpn, unit_type, unit_race):
     created_json = {
         "name": name,
         "type": unit_type,
         "race": unit_race,
         "stage": "Base",
         "base_weapon_ranks": {
-            weapon: "D"
+            wpn: "D"
         },
         "max_weapon_ranks": {
-            weapon: "A"
+            wpn: "A"
         },
         "movement": stats[-1],
         "base_stats": {
@@ -119,28 +116,43 @@ def create_class_json(name, stats, growthList, weapon, unit_type, unit_race):
             "con": 0
         },
         "growth_rates": {
-            "hp": 80,
-            "str/mag": 40,
-            "skl": 40,
-            "spd": 32,
-            "lck": 30,
-            "def": 18,
-            "res": 15,
+            "hp": growths[0],
+            "str/mag": growths[1],
+            "skl": growths[2],
+            "spd": growths[3],
+            "lck": growths[-1],
+            "def": growths[4],
+            "res": growths[5],
             "con": 0
         }
     }
 
-    return created_json
+    return json.dumps(created_json)
 
 
-# collection name and json to be sent
+# URL and database collection name being used
+URL = "https://serenesforest.net/binding-blade/classes/base-stats/"
 collection_name = "Classes"
-growthList = []
-send_json = create_class_json(nameList[-2], statList, growthList)
 
-# test cases to know where everything is
+# testing information
+# growthList will be replaced with the actual growths data
+# weapon, unitType, and raceType will be user input
+growthList = [50, 50, 50, 50, 50, 50, 50]
+weapon = "swords"
+unitType = "Infantry"
+unitRace = "Human"
+
+# call the functions to get the data and create a json
+nameList, statList = get_data_from_page(URL)
+send_json = create_class_json(nameList[-2], statList, growthList, weapon, unitType, unitRace)
+
+# call the function to add the data to the database
+# send_json_to_mongodb(send_json, collection_name)
+
+# test cases to know where everything is, its format, and its
 print(nameList)
 print(statList)
+print(send_json)
 
 y = 1
 k = 1
